b0a952189ad70d0d92cadeeb750ea7238f4f22dd
olutoba obawemimo <tobbyoba86@gmail.com>
2023-09-25 18:06:29 -0500

    first commit

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "s3-backed" {
-  source = "git::https://github.com/tobbyoba/Gitaction_s3_backed.git//module-making?ref=v1.1.0"
+  source = "git@github.com:tobbyoba/Gitaction_s3_backed.git//module-making?ref=v1.1.0"
   region = "us-east-2"
   env    = "staging"
   versioning = "Enabled"
 }
 
+#terraform
 terraform {
   backend "s3" {
     bucket = "my-s3testing-bucket"
