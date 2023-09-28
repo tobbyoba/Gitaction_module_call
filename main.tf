83be10ed3c00dad78e7f6c68e84e61c972d824e0
tobbyoba <tobbyoba86@gmail.com>
2023-09-27 22:58:24 -0500

    deploy key

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ terraform {
     encrypt = "true"
   }
 }
-provider "aws" {
-  region = "var.region"
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
